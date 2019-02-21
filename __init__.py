086311ef9ac9a99f4eb11caf90dd9136ae68dab2
dcereijodo <dcereijo@digitalorigin.com>
2019-02-21 16:45:56 +0100

    bug: forgot in last commit

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
 from airflow.plugins_manager import AirflowPlugin
 from redshift_plugin.operators.s3_to_redshift_operator import S3ToRedshiftOperator
-from redshift_plugin.macros.redshift_auth import redshift_auth
-
 
 class S3ToRedshiftPlugin(AirflowPlugin):
     name = "S3ToRedshiftPlugin"
@@ -9,7 +7,7 @@ class S3ToRedshiftPlugin(AirflowPlugin):
     # Leave in for explicitness
     hooks = []
     executors = []
-    macros = [redshift_auth]
+    macros = []
     admin_views = []
     flask_blueprints = []
     menu_links = []
