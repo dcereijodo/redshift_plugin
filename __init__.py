b0b81908a2848421b0a34714d59e502de122d88e
dcereijodo <dcereijo@digitalorigin.com>
2019-05-31 12:09:13 +0200

    use a more pythonic name for pluging module

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from airflow.plugins_manager import AirflowPlugin
 from redshift_plugin.operators.s3_to_redshift_operator import S3ToRedshiftOperator
 
 class S3ToRedshiftPlugin(AirflowPlugin):
-    name = "S3ToRedshiftPlugin"
+    name = "redshift_plugin"
     operators = [S3ToRedshiftOperator]
     # Leave in for explicitness
     hooks = []
